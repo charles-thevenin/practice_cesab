d44582f1b7d8f72d47fe39ee831cdb2f9eb89e1a
Charles Thévenin <charles.thevenin@mnhn.fr>
2025-12-02 10:16:20 +0100

    add comments to download-data.R

--- analyses/download-data.R
+++ analyses/download-data.R
@@ -4,6 +4,12 @@
 #
 ######################################
 
+# Lister les noms de fichiers à telecharger
+wildfinder_files <- c("wildfinder-ecoregions_list.csv",
+"wildfinder-ecoregions_list.csv",
+"wildfinder-mammals_list.csv")
+
+# Utiliser la fonction dl_pantheria_data() pour télécharger les fichiers depuis le repo wildfinder
 for(i in c("wildfinder-ecoregions_list.csv", "wildfinder-ecoregions_list.csv", "wildfinder-mammals_list.csv"))
   {
   dl_pantheria_data(filename = i)
